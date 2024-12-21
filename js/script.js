04b3942376a11f4b30b45f54e5fda6bb9e62febb
ojasvsingh71 <ojasvsingh0@gmail.com>
2024-12-21 15:26:41 +0530

    Done

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
-// Add Zoom Effect on Image Hover
 const images = document.querySelectorAll("img");
 
 images.forEach(image => {
   image.addEventListener("mouseover", () => {
-    image.style.transform = "scale(1.1)";  // Zoom in effect
-    image.style.transition = "transform 0.3s ease";  // Smooth transition
+    image.style.transform = "scale(1.1)";  
+    image.style.transition = "transform 0.3s ease";  
   });
 
   image.addEventListener("mouseout", () => {
-    image.style.transform = "scale(1)";  // Reset the image to original size
+    image.style.transform = "scale(1)";  
   });
 });
